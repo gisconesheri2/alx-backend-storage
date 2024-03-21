b54bdc040b4f5453ec72773e89c24aa6c56da11e
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-21 11:50:29 +0300

    use enum in table creation

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
--- create a table user on any database
+-- use enum in table creation
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(255) NOT NULL,
     `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') DEFAULT 'US'
     PRIMARY KEY (`id`),
     UNIQUE (`email`)
 );
