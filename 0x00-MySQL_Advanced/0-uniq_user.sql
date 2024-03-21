2dad26a005f539f748264257fd74c855c38b646e
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-21 11:38:06 +0300

    create table sql

--- 0x00-MySQL_Advanced/0-uniq_user.sql
+++ 0x00-MySQL_Advanced/0-uniq_user.sql
@@ -1,7 +1,8 @@
 -- create a table user on any database
-CREATE TABLE users IF NOT EXISTS (
-    id INT NOT NULL AUTO-INCREMENT,
-    email CHAR(255) NOT NULL UNIQUE,
-    name CHAR(255),
-    PRIMARY KEY (id)
-)
+CREATE TABLE `users` IF NOT EXISTS (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255),
+    PRIMARY KEY (`id`),
+    UNIQUE (`email`)
+);
