0e4dc0a4e904a55648d7a2c64d744fc55d6b928b
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-21 15:22:22 +0300

    create new procedures

--- 0x00-MySQL_Advanced/0-uniq_user.sql
+++ 0x00-MySQL_Advanced/0-uniq_user.sql
@@ -1,8 +1,7 @@
--- create a table user on any database
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL,
-    `name` VARCHAR(255),
-    PRIMARY KEY (`id`),
-    UNIQUE (`email`)
+-- Creates users table.
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
 );
