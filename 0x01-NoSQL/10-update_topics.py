eef2e771a8cd9d83d46f9ec1f0a481b0830276f6
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-26 20:59:48 +0300

    update doc in a collection in python

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+update a document in a collection
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update a document in a collection
+    """
+    filter = {'school': name}
+    new_values = {"$set": {'topics': topics}}
+
+    mongo_collection.update_one(filter, new_values)
