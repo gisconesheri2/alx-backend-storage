db048774a5ac32c1b630c0873b0201fc52a51ad4
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-26 22:32:27 +0300

    parse nginx log files

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -9,8 +9,6 @@ def update_topics(mongo_collection, name, topics):
     update a document in a collection
     """
     filter = {'name': name}
-    print(filter)
     new_values = {"$set": {'topics': topics}}
-    print(new_values)
 
     mongo_collection.update_one(filter, new_values)
