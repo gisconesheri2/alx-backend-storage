6a5d9beedacecc91e9c6ed6b45a0cc904b3d4cf9
gisconesheri2 <pgikonyo2022@gmail.com>
2024-03-27 22:12:40 +0300

    store function calls in redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,49 @@ use a redis client to store a value to the database
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Count number of times a function is called"""
+
+    @wraps(method)
+    def increment_call(self, *args, **kwargs):
+        """Increments the value associated with methond name"""
+        self._redis.incr(method.__qualname__, amount=1)
+        return method(self, *args, **kwargs)
+    return increment_call
+
+
+def call_history(method: Callable) -> Callable:
+    """add inputs and outputs of method to
+    method's respective fields"""
+
+    m_inp = "{}:inputs".format(method.__qualname__)
+    m_out = "{}:outputs".format(method.__qualname__)
+
+    @wraps(method)
+    def add_args(self, *args, **kwargs):
+        """push to funtion inputs and outputs fields"""
+
+        self._redis.rpush(m_inp, str(args))
+        out = method(self, *args, **kwargs)
+        self._redis.rpush(m_out, str(out))
+        return out
+    return add_args
+
+
+def replay(method: Callable) -> None:
+    r = redis.Redis()
+    calls = r.get(method.__qualname__)
+    print('{} was called {} times:'.format(method, calls))
+
+    inputs =r.lrange("{}:inputs".format(method.__qualname__), 0, -1)
+    outputs = r.lrange("{}:outputs".format(method.__qualname__), 0, -1)
+    io = zip(inputs, outputs)
+    
+    for i, o in io:
+        print('{}(*({})) -> {}'.format(method, i, o))
 
 
 class Cache():
@@ -14,12 +57,14 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store a value in a redis server"""
         k_str = str(uuid.uuid4())
         self._redis.set(k_str, data)
         return k_str
-    
+
     def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, float, str, bytes]:
         """get the value associated with key"""
@@ -30,7 +75,7 @@ class Cache():
             else:
                 return val
         return val
-    
+
     def get_str(self, key: str) -> str:
         """
         convert value from the database
@@ -40,7 +85,7 @@ class Cache():
         if val:
             val.decode('utf-8')
         return val
-    
+
     def get_int(self, key: str) -> Union[int, None]:
         """
         convert value from the database
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
+""" Main file """
 
 Cache = __import__('exercise').Cache
+replay = __import__('exercise').replay
 
 cache = Cache()
 
-cache = Cache()
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
 
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
+
+replay(cache.store)
